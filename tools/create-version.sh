80bd211f98f196fdd09f58c88734a13e5ec3b9ee
Jake Willmsen <jdwillmsen@gmail.com>
2024-03-06 21:48:01 -0600

    feat: add git commit in create_version script

--- tools/create-version.sh
+++ tools/create-version.sh
@@ -2,18 +2,19 @@
 
 # Script: create-version.sh
 # Description: Creates a version file with the specified version number at the provided path.
-# Usage: ./create-version.sh <version_number> <file_path>
+# Usage: ./create-version.sh <version_number> <file_path> <project_name>
 
 # Check if correct number of arguments are provided
-if [ "$#" -lt 2 ]; then
+if [ "$#" -lt 3 ]; then
   echo "hi"
-    echo "Usage: $0 <version_number> <file_path>"
+    echo "Usage: $0 <version_number> <file_path> <project_name>"
     exit 1
 fi
 
 # Assign arguments to variables
 version=$1
 path=$2
+project_name=$3
 
 # Create directory if it doesn't exist
 mkdir -p $path
@@ -22,4 +23,8 @@ mkdir -p $path
 file_path="$path/VERSION"
 echo "$version" > "$file_path"
 
-echo "Version file for version $version created at $file_path"
+git add $file_path
+git commit -m "chore($project_name): update project version file to version $version"
+git push
+
+echo "[$project_name]: Version file for version $version created at $file_path"
