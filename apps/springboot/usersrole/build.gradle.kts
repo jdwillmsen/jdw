82dad19d9c9b21f0dd526b317558a882e23189ef
Jake Willmsen <jdwillmsen@gmail.com>
2024-05-13 00:24:34 -0500

    build(usersrole): update plugins dependencies

--- apps/springboot/usersrole/build.gradle.kts
+++ apps/springboot/usersrole/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.2.5"
 	id("io.spring.dependency-management") version "1.1.4"
-	id("com.diffplug.spotless") version "6.25.0"
 	id("maven-publish")
 }
 
