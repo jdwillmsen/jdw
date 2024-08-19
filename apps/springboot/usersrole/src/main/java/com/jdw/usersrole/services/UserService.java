87142642be6e21c2d4925103a7485281635e5e1c
Jake Willmsen <jdwillmsen@gmail.com>
2024-08-18 21:53:57 -0500

    feat(usersrole): add saving of the initiator of users requests

--- apps/springboot/usersrole/src/main/java/com/jdw/usersrole/services/UserService.java
+++ apps/springboot/usersrole/src/main/java/com/jdw/usersrole/services/UserService.java
@@ -41,8 +41,19 @@ public class UserService {
                 .orElseThrow(() -> new ResourceNotFoundException("User not found with email address " + emailAddress));
     }
 
-    public User createUser(@Valid UserRequestDTO userDTO) {
+    public User createUser(@NotNull @Valid UserRequestDTO userDTO) {
         log.info("Creating user: {}", userDTO);
+        return createUser(userDTO, 1L);
+    }
+
+    public User createUser(@NotNull @Valid UserRequestDTO userDTO, @NotNull String emailAddress) {
+        log.info("Creating user: user={}, requester={}", userDTO, emailAddress);
+        Long userId = getUserIdByEmailAddress(emailAddress);
+        return createUser(userDTO, userId);
+    }
+
+    public User createUser(@NotNull @Valid UserRequestDTO userDTO, @NotNull Long userId) {
+        log.info("Creating user: user={}, requesterId={}", userDTO, userId);
         userRepository.findByEmailAddress(userDTO.emailAddress())
                 .ifPresent(user -> {
                     throw new ResourceExistsException("User already exists with email address " + userDTO.emailAddress());
@@ -53,18 +64,19 @@ public class UserService {
                 .password(passwordEncoder.encode(userDTO.password()))
                 .status(Status.ACTIVE.name())
                 .roles(null)
-                .createdByUserId(1L)
+                .createdByUserId(userId)
                 .createdTime(currentTime)
-                .modifiedByUserId(1L)
+                .modifiedByUserId(userId)
                 .modifiedTime(currentTime)
                 .build();
         return userRepository.save(newUser);
     }
 
-    public User updateUser(@NotNull Long id, @Valid UserRequestDTO userDTO) {
-        log.info("Updating user: {}", userDTO);
+    public User updateUser(@NotNull Long id, @Valid UserRequestDTO userDTO, @NotNull String emailAddress) {
+        log.info("Updating user: user={}, requester={}", userDTO, emailAddress);
         User currentUser = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found with id " + id));
+        Long userId = getUserIdByEmailAddress(emailAddress);
         Timestamp currentTime = Timestamp.from(Instant.now());
         User updatedUser = User.builder()
                 .id(id)
@@ -75,14 +87,19 @@ public class UserService {
                 .profile(currentUser.profile())
                 .createdByUserId(currentUser.createdByUserId())
                 .createdTime(currentUser.createdTime())
-                .modifiedByUserId(1L)
+                .modifiedByUserId(userId)
                 .modifiedTime(currentTime)
                 .build();
         return userRepository.save(updatedUser);
     }
 
-    public void deleteUser(@NotNull Long id) {
-        log.info("Deleting user: {}", id);
+    public void deleteUser(@NotNull Long id, @NotNull String emailAddress) {
+        log.info("Deleting user: id={}, requester={}", id, emailAddress);
         userRepository.deleteById(id);
+    }
+
+    public Long getUserIdByEmailAddress(@NotNull String emailAddress) {
+        log.info("Getting user id with email address: {}", emailAddress);
+        return getUserByEmailAddress(emailAddress).id();
     }
 }
