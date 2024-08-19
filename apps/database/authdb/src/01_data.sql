74098689199d8ace26766f8199b97df79bd577c3
Jake Willmsen <jdwillmsen@gmail.com>
2024-08-18 21:18:39 -0500

    feat(authdb): update database init data

--- apps/database/authdb/src/01_data.sql
+++ apps/database/authdb/src/01_data.sql
@@ -8,7 +8,7 @@ INSERT INTO auth.roles (role_name, role_description, created_by_user_id, created
 VALUES ('USER', 'Standard user role with basic access.', 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP);
 
 INSERT INTO auth.users (email_address, password, created_by_user_id, created_time, modified_by_user_id, modified_time)
-VALUES ('system@jdw.com', '$2a$12$5bvRmlbfiLYEkJ9p6kS4ouFibRGx1BYLXvgXyn3dBQP6TpZAVOzCa', 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP);
+VALUES ('system@jdw.com', '$2a$10$v5e3IStWiGNj1tmB8WWoguoVQHFThHyMlqaOJP1vrNyrrcZfwDRBa', 1, CURRENT_TIMESTAMP, 1, CURRENT_TIMESTAMP);
 
 INSERT INTO auth.users_roles (user_id, role_id, created_by_user_id, created_time)
 VALUES (1, 1, 1, CURRENT_TIMESTAMP),  -- Admin role
