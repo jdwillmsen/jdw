62aac8591a99e1b836332b743c5dc5069d63d155
Jake Willmsen <jdwillmsen@gmail.com>
2024-08-18 21:20:08 -0500

    feat(usersrole): add non locked down create user endpoint

--- apps/springboot/usersrole/src/main/java/com/jdw/usersrole/controllers/AuthController.java
+++ apps/springboot/usersrole/src/main/java/com/jdw/usersrole/controllers/AuthController.java
@@ -1,10 +1,13 @@
 package com.jdw.usersrole.controllers;
 
 import com.jdw.usersrole.dtos.UserRequestDTO;
+import com.jdw.usersrole.models.User;
 import com.jdw.usersrole.services.AuthService;
+import com.jdw.usersrole.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,11 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class AuthController {
     private final AuthService authService;
+    private final UserService userService;
 
     @PostMapping("/authenticate")
     public ResponseEntity<String> authenticate(@Valid @RequestBody UserRequestDTO user) {
         log.trace("Authenticating user {}", user);
         String token = authService.authenticate(user);
         return ResponseEntity.ok(token);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<User> createUser(@Valid @RequestBody UserRequestDTO user) {
+        log.trace("Creating user {}", user);
+        User userCreated = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
