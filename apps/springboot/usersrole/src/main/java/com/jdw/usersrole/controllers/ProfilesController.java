c066cb5e1ce078c8466381411abbf2c1a1baa8a7
Jake Willmsen <jdwillmsen@gmail.com>
2024-08-18 21:21:54 -0500

    feat(usersrole): add pre-authorize to endpoints for locking them down to specific user roles

--- apps/springboot/usersrole/src/main/java/com/jdw/usersrole/controllers/ProfilesController.java
+++ apps/springboot/usersrole/src/main/java/com/jdw/usersrole/controllers/ProfilesController.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,14 +22,16 @@ import java.util.List;
 @RequestMapping("/api/profiles")
 @Slf4j
 public class ProfilesController {
-     private final ProfileService profileService;
+    private final ProfileService profileService;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public ResponseEntity<List<Profile>> getProfiles() {
         log.trace("Getting all profiles");
         return ResponseEntity.ok(profileService.getProfiles());
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or #profileId == authentication.principal.getProfileId()")
     @GetMapping("/{profileId}")
     public ResponseEntity<Profile> getProfileById(@PathVariable Long profileId) {
         log.trace("Getting profile with id {}", profileId);
@@ -36,6 +39,7 @@ public class ProfilesController {
         return ResponseEntity.ok(profile);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or #userId == authentication.principal.getUserId()")
     @GetMapping("/user/{userId}")
     public ResponseEntity<Profile> getProfileByUserId(@PathVariable Long userId) {
         log.trace("Getting profile with user id {}", userId);
@@ -43,6 +47,7 @@ public class ProfilesController {
         return ResponseEntity.ok(profile);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or #profile.userId() == authentication.principal.getUserId()")
     @PostMapping
     public ResponseEntity<Profile> createProfile(@Valid @RequestBody ProfileCreateRequestDTO profile) {
         log.trace("Creating profile {}", profile);
@@ -50,6 +55,7 @@ public class ProfilesController {
         return ResponseEntity.status(HttpStatus.CREATED).body(createdProfile);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or #profileId == authentication.principal.getProfileId()")
     @PutMapping("/{profileId}")
     public ResponseEntity<Profile> updateProfileById(@PathVariable Long profileId,
                                                      @Valid @RequestBody ProfileUpdateRequestDTO profile) {
@@ -58,6 +64,7 @@ public class ProfilesController {
         return ResponseEntity.ok(updatedProfile);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or #userId == authentication.principal.getUserId()")
     @PutMapping("/user/{userId}")
     public ResponseEntity<Profile> updateProfileByUserId(@PathVariable Long userId,
                                                          @Valid @RequestBody ProfileUpdateRequestDTO profile) {
@@ -66,6 +73,7 @@ public class ProfilesController {
         return ResponseEntity.ok(updatedProfile);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or #profileId == authentication.principal.getProfileId()")
     @DeleteMapping("/{profileId}")
     public ResponseEntity<Profile> deleteProfileById(@PathVariable Long profileId) {
         log.trace("Deleting profile with id {}", profileId);
@@ -73,6 +81,7 @@ public class ProfilesController {
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or #userId == authentication.principal.getUserId()")
     @DeleteMapping("/user/{userId}")
     public ResponseEntity<Profile> deleteProfileByUserId(@PathVariable Long userId) {
         log.trace("Deleting profile with user id {}", userId);
@@ -80,6 +89,7 @@ public class ProfilesController {
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or #profileId == authentication.principal.getProfileId()")
     @PostMapping("/{profileId}/address")
     public ResponseEntity<Profile> addAddress(@PathVariable Long profileId,
                                               @Valid @RequestBody AddressRequestDTO address) {
@@ -88,6 +98,7 @@ public class ProfilesController {
         return ResponseEntity.ok(updatedProfile);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or #profileId == authentication.principal.getProfileId()")
     @PutMapping("/{profileId}/address/{addressId}")
     public ResponseEntity<Profile> updateAddress(@PathVariable Long profileId,
                                                  @PathVariable Long addressId,
@@ -97,6 +108,7 @@ public class ProfilesController {
         return ResponseEntity.ok(updatedProfile);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or #profileId == authentication.principal.getProfileId()")
     @DeleteMapping("/{profileId}/address/{addressId}")
     public ResponseEntity<Profile> deleteAddress(@PathVariable Long profileId, @PathVariable Long addressId) {
         log.trace("Deleting address with id {}", addressId);
@@ -104,12 +116,14 @@ public class ProfilesController {
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping(value ="/{profileId}/icon", produces = MediaType.IMAGE_PNG_VALUE)
+    @PreAuthorize("hasAuthority('ADMIN') or #profileId == authentication.principal.getProfileId()")
+    @GetMapping(value = "/{profileId}/icon", produces = MediaType.IMAGE_PNG_VALUE)
     public ResponseEntity<byte[]> getProfileIcon(@PathVariable Long profileId) {
         log.trace("Getting profile icon with id {}", profileId);
         return ResponseEntity.ok(profileService.getIcon(profileId));
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or #profileId == authentication.principal.getProfileId()")
     @PostMapping("/{profileId}/icon")
     public ResponseEntity<Profile> addIcon(@PathVariable Long profileId, @RequestParam("icon") MultipartFile file) {
         log.trace("Adding icon with id {}", profileId);
@@ -117,6 +131,7 @@ public class ProfilesController {
         return ResponseEntity.ok(updatedProfile);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or #profileId == authentication.principal.getProfileId()")
     @PutMapping("/{profileId}/icon")
     public ResponseEntity<Profile> updateIcon(@PathVariable Long profileId, @RequestParam("icon") MultipartFile file) {
         log.trace("Updating icon with id {}", profileId);
@@ -124,6 +139,7 @@ public class ProfilesController {
         return ResponseEntity.ok(updatedProfile);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or #profileId == authentication.principal.getProfileId()")
     @DeleteMapping("/{profileId}/icon")
     public ResponseEntity<Profile> deleteIcon(@PathVariable Long profileId) {
         log.trace("Deleting icon with id {}", profileId);
