e1693a5a006cee15a5f71604698b757b47fc5869
Jake Willmsen <jdwillmsen@gmail.com>
2024-09-01 00:51:30 -0500

    fix(usersrole): fix role service

--- apps/springboot/usersrole/src/main/java/com/jdw/usersrole/services/RoleService.java
+++ apps/springboot/usersrole/src/main/java/com/jdw/usersrole/services/RoleService.java
@@ -11,7 +11,6 @@ import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Timestamp;
 import java.time.Instant;
@@ -84,61 +83,6 @@ public class RoleService {
 
     public void deleteRole(@NotNull Long id, @NotNull String emailAddress) {
         log.info("Deleting role with: id={}, requester={}", id, emailAddress);
-        roleRepository.deleteById(id);
-    }
-
-    public Role getRoleByName(String name) {
-        log.info("Getting role with name {}", name);
-        return roleRepository.findByName(name).orElse(null);
-    }
-
-    public Role createRole(Role role) {
-        log.info("Creating role {}", role);
-        Timestamp currentTime = Timestamp.from(Instant.now());
-        Role newRole = Role.builder()
-                .name(role.name())
-                .description(role.description())
-                .status(Status.ACTIVE.name())
-                .users(null)
-                .createdByUserId(1L)
-                .createdTime(currentTime)
-                .modifiedByUserId(1L)
-                .modifiedTime(currentTime)
-                .build();
-        return roleRepository.save(newRole);
-    }
-
-    public Role updateRole(Long id, Role role) {
-        log.info("Updating role: id={}, role={}", id, role);
-        if (role == null
-                || id == null
-                || role.name() == null
-                || role.description() == null
-                || !roleRepository.existsById(id)) {
-            return null;
-        }
-        Role currentRole = roleRepository.findById(id).orElse(null);
-        if (currentRole == null) {
-            return null;
-        }
-        String status = role.status() != null ? role.status() : Status.ACTIVE.name();
-        Timestamp currentTime = Timestamp.from(Instant.now());
-        Role updatedRole = Role.builder()
-                .id(id)
-                .name(role.name())
-                .description(role.description())
-                .status(status)
-                .users(currentRole.users())
-                .createdByUserId(currentRole.createdByUserId())
-                .createdTime(currentRole.createdTime())
-                .modifiedByUserId(1L)
-                .modifiedTime(currentTime)
-                .build();
-        return roleRepository.save(updatedRole);
-    }
-
-    public void deleteRole(Long id) {
-        log.info("Deleting role with id {}", id);
         roleRepository.deleteById(id);
     }
 }
