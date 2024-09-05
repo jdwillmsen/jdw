855a32a3b135604abadf30ec6e1e52c6f457a9a2
Jake Willmsen <jdwillmsen@gmail.com>
2024-09-05 00:37:12 -0500

    test(usersrole): add random port to users role application integration test

--- apps/springboot/usersrole/src/test/java/com/jdw/usersrole/UsersRoleApplicationTests.java
+++ apps/springboot/usersrole/src/test/java/com/jdw/usersrole/UsersRoleApplicationTests.java
@@ -3,12 +3,14 @@ package com.jdw.usersrole;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @Tag("fast")
 @Tag("integration")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@TestPropertySource(properties = "server.port=0")
 class UsersRoleApplicationTests {
     @Test
     void contextLoads() {
