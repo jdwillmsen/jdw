7c9d68eb5b92d202d4e39e0599f2649e0cc683ad
Jake Willmsen <jdwillmsen@gmail.com>
2024-09-08 23:29:28 -0500

    feat(authui): add authui application

--- apps/angular/container/container/src/app/app.routes.ts
+++ apps/angular/container/container/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Route } from '@angular/router';
 import { angularContainerFeatureCoreRoutes } from '@jdw/angular-container-feature-core';
+import { loadRemoteModule } from '@nx/angular/mf';
 
 export const appRoutes: Route[] = [
+  {
+    path: 'authui',
+    loadChildren: () =>
+      loadRemoteModule('authui', './Routes').then((m) => m.remoteRoutes),
+  },
   {
     path: '',
     children: angularContainerFeatureCoreRoutes,
