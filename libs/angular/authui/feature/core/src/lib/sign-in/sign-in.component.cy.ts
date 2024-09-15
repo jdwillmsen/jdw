d340e5b062af887d3c55cdd24143be89b872def0
Jake Willmsen <jdwillmsen@gmail.com>
2024-09-15 18:32:21 -0500

    test(angular-authui-feature-core): add sign-in component tests

---
+++ libs/angular/authui/feature/core/src/lib/sign-in/sign-in.component.cy.ts
@@ -0,0 +1,50 @@
+import { TestBed } from '@angular/core/testing';
+import { SignInComponent } from './sign-in.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+
+describe(SignInComponent.name, () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [BrowserAnimationsModule],
+    }).overrideComponent(SignInComponent, {
+      add: {
+        imports: [MatSnackBarModule],
+        providers: [
+          {
+            provide: ActivatedRoute,
+            useValue: {},
+          },
+        ],
+      },
+    });
+  });
+
+  it('renders', () => {
+    cy.mount(SignInComponent);
+  });
+
+  describe('Screen Sizes', () => {
+    testScreenSize('XSmall', 400, 400);
+    testScreenSize('Small', 800, 800);
+    testScreenSize('Medium', 1200, 900);
+    testScreenSize('Large', 1600, 1080);
+    testScreenSize('XLarge', 2560, 1440);
+  });
+});
+
+function testScreenSize(size: string, width: number, height: number) {
+  it(`should be setup properly on ${size} screen size`, () => {
+    cy.viewport(width, height);
+    cy.mount(SignInComponent);
+    cy.getByCy('title').should('be.visible').and('contain.text', 'Sign In');
+    cy.getByCy('email-sign-in').should('be.visible');
+    cy.getByCy('new-user')
+      .should('be.visible')
+      .and('contain.text', 'New User?');
+    cy.getByCy('sign-up-link')
+      .should('be.visible')
+      .and('contain.text', 'Sign up');
+  });
+}
