d4252bf19fa2ad16b85c7e60de9c88e7f6e03d91
Jake Willmsen <jdwillmsen@gmail.com>
2024-09-15 00:37:45 -0500

    feat(angular-authui-feature-core): add email-sign-in component and updates to sign-in

---
+++ libs/angular/authui/feature/core/src/lib/email-sign-in/email-sign-in.component.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatError, MatFormField } from '@angular/material/form-field';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import {
+  EMAIL_PATTERN_VALIDATION_MESSAGE,
+  EMAIL_REQUIRED_VALIDATION_MESSAGE,
+  EMAIL_VALIDATOR_PATTERN,
+  PASSWORD_MIN_LENGTH_VALIDATION_MESSAGE,
+  PASSWORD_REQUIRED_VALIDATION_MESSAGE,
+} from '@jdw/angular-authui-util';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'lib-email-sign-in',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './email-sign-in.component.html',
+  styleUrl: './email-sign-in.component.scss',
+})
+export class EmailSignInComponent {
+  form: FormGroup = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern(EMAIL_VALIDATOR_PATTERN),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+  hide = true;
+  validationMessages = {
+    email: [
+      { type: 'required', message: EMAIL_REQUIRED_VALIDATION_MESSAGE },
+      { type: 'pattern', message: EMAIL_PATTERN_VALIDATION_MESSAGE },
+    ],
+    password: [
+      { type: 'required', message: PASSWORD_REQUIRED_VALIDATION_MESSAGE },
+      {
+        type: 'minlength',
+        message: PASSWORD_MIN_LENGTH_VALIDATION_MESSAGE,
+      },
+    ],
+  };
+
+  signIn() {
+    alert('Sign in clicked!');
+  }
+
+  getErrorMessage(formControlName: 'email' | 'password') {
+    for (const validation of this.validationMessages[formControlName]) {
+      if (this.form.get(formControlName)?.hasError(validation.type)) {
+        return validation.message;
+      }
+    }
+    return '';
+  }
+}
