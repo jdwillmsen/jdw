ca9174748a3b00efbde39fdb0b016cea2020545d
Jake Willmsen <jdwillmsen@gmail.com>
2024-09-15 18:23:32 -0500

    test(angular-authui-feature-core): add email-sign-in component tests

---
+++ libs/angular/authui/feature/core/src/lib/email-sign-in/email-sign-in.component.cy.ts
@@ -0,0 +1,81 @@
+import { TestBed } from '@angular/core/testing';
+import { EmailSignInComponent } from './email-sign-in.component';
+import { Route } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+describe(EmailSignInComponent.name, () => {
+  beforeEach(() => {
+    @Component({
+      selector: 'lib-test-home',
+      standalone: true,
+      template: '<div>Test Home Component Works!</div>',
+    })
+    class TestHomeComponent {}
+    const routes: Route[] = [
+      {
+        path: 'home',
+        component: TestHomeComponent,
+      },
+    ];
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule,
+        RouterTestingModule.withRoutes(routes),
+      ],
+    }).overrideComponent(EmailSignInComponent, {
+      add: {
+        imports: [MatSnackBarModule],
+        providers: [],
+      },
+    });
+  });
+
+  it('renders', () => {
+    cy.mount(EmailSignInComponent);
+  });
+
+  it('should be setup properly', () => {
+    cy.mount(EmailSignInComponent);
+    cy.getByCy('email-address-field')
+      .should('be.visible')
+      .and('contain.text', 'Enter your email');
+    cy.getByCy('password-field')
+      .should('be.visible')
+      .and('contain.text', 'Enter your password');
+    cy.getByCy('password-visibility-button')
+      .should('be.visible')
+      .and('be.enabled')
+      .click();
+    cy.getByCy('sign-in-button')
+      .should('be.visible')
+      .and('contain.text', 'Sign In')
+      .and('be.enabled');
+  });
+
+  it('should show error fields for improper input', () => {
+    cy.mount(EmailSignInComponent);
+    cy.getByCy('email-address-field').find('input').click().blur();
+    cy.getByCy('email-address-field')
+      .should('contain.text', 'Email is required')
+      .type('test')
+      .should('contain.text', 'Enter a valid email');
+    cy.getByCy('password-field').find('input').click().blur();
+    cy.getByCy('password-field')
+      .should('contain.text', 'Password is required')
+      .type('test')
+      .should('contain.text', 'Password must be at least 6 characters long');
+  });
+
+  it('should submit form successfully with correct input', () => {
+    cy.mount(EmailSignInComponent);
+    cy.getByCy('email-address-field').type('test-user-1@usersrole.com');
+    cy.getByCy('password-field').type('testPassword');
+    cy.getByCy('sign-in-button').click();
+    // cy.getByCy('snackbar-container')
+    //   .should('be.visible')
+    //   .and('contain.text', 'Sign in successful');
+  });
+});
