2f4288fbc64ba3b2012039f5bd54b77a18bb1b40
Jake Willmsen <jdwillmsen@gmail.com>
2024-09-15 00:37:07 -0500

    feat(angular-authui-util): add validator constants

--- libs/angular/authui/util/src/lib/validator.constant.ts
+++ libs/angular/authui/util/src/lib/validator.constant.ts
@@ -1,2 +1,7 @@
+export const PASSWORD_MIN_LENGTH_VALIDATION_MESSAGE =
+  'Password must be at least 6 characters long';
+export const EMAIL_REQUIRED_VALIDATION_MESSAGE = 'Email is required';
+export const EMAIL_PATTERN_VALIDATION_MESSAGE = 'Enter a valid email';
+export const PASSWORD_REQUIRED_VALIDATION_MESSAGE = 'Password is required';
 export const EMAIL_VALIDATOR_PATTERN =
   '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}';
