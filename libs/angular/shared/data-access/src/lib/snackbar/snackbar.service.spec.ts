9ac31027b24e990849231bb681aae6716f15dca3
Jake Willmsen <jdwillmsen@gmail.com>
2024-09-21 17:50:54 -0500

    feat(angular-shared-data-access): add snackbar service

---
+++ libs/angular/shared/data-access/src/lib/snackbar/snackbar.service.spec.ts
@@ -0,0 +1,474 @@
+import { TestBed } from '@angular/core/testing';
+
+import { SnackbarService } from './snackbar.service';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+import { PaletteColors, SnackbarOptions } from '@jdw/angular-shared-util';
+import { SnackbarComponent } from '@jdw/angular-shared-ui';
+
+describe('SnackbarService', () => {
+  let service: SnackbarService;
+
+  const snackbarMock: jest.Mocked<any> = {
+    openFromComponent: jest.fn(),
+    dismiss: jest.fn(),
+  };
+  const defaultType: PaletteColors = 'primary';
+  const defaultMessage = 'test message';
+  const defaultDuration = 3000;
+  const defaultHorizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  const defaultVerticalPosition: MatSnackBarVerticalPosition = 'bottom';
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: MatSnackBar,
+          useValue: snackbarMock,
+        },
+      ],
+    });
+    service = TestBed.inject(SnackbarService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should create an instance of SnackbarService', () => {
+    expect(service).toBeInstanceOf(SnackbarService);
+  });
+
+  it('should send a snackbar', () => {
+    service.send(defaultType, defaultMessage);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: undefined,
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', defaultType],
+      },
+    );
+  });
+
+  it('should send a snackbar with all options', () => {
+    const options: SnackbarOptions = {
+      variant: 'filled',
+      autoClose: true,
+      autoCloseTimeout: 5000,
+      icon: 'bug_report',
+      buttonText: 'Test',
+      direction: 'rtl',
+      horizontalPosition: 'start',
+      verticalPosition: 'top',
+    };
+
+    service.send(defaultType, defaultMessage, options);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: options.icon,
+          buttonText: options.buttonText,
+        },
+        direction: options.direction,
+        duration: options.autoCloseTimeout,
+        horizontalPosition: options.horizontalPosition,
+        verticalPosition: options.verticalPosition,
+        panelClass: [options.variant, defaultType],
+      },
+    );
+  });
+
+  it('should send a snackbar with some options', () => {
+    const options: SnackbarOptions = {
+      variant: 'default',
+      autoClose: true,
+    };
+
+    service.send(defaultType, defaultMessage, options);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: undefined,
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: defaultDuration,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', options.variant, defaultType],
+      },
+    );
+  });
+
+  it('should send a success snackbar', () => {
+    service.success(defaultMessage);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: undefined,
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', 'success'],
+      },
+    );
+  });
+
+  it('should send a success snackbar with defaultIcon', () => {
+    service.success(defaultMessage, undefined, true);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: 'check_circle',
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', 'success'],
+      },
+    );
+  });
+
+  it('should send a success snackbar with options', () => {
+    const options: SnackbarOptions = {
+      variant: 'outlined',
+      icon: 'bug_report',
+    };
+    service.success(defaultMessage, options, false);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: options.icon,
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', options.variant, 'success'],
+      },
+    );
+  });
+
+  it('should send a success snackbar with default icon overriding options', () => {
+    const options: SnackbarOptions = {
+      icon: 'bug_report',
+    };
+    service.success(defaultMessage, options, true);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: 'check_circle',
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', 'success'],
+      },
+    );
+  });
+
+  it('should send an error snackbar', () => {
+    service.error(defaultMessage);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: undefined,
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', 'error'],
+      },
+    );
+  });
+
+  it('should send an error snackbar with defaultIcon', () => {
+    service.error(defaultMessage, undefined, true);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: 'report',
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', 'error'],
+      },
+    );
+  });
+
+  it('should send an error snackbar with options', () => {
+    const options: SnackbarOptions = {
+      variant: 'outlined',
+      icon: 'bug_report',
+    };
+    service.error(defaultMessage, options, false);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: options.icon,
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', options.variant, 'error'],
+      },
+    );
+  });
+
+  it('should send an error snackbar with default icon overriding options', () => {
+    const options: SnackbarOptions = {
+      icon: 'bug_report',
+    };
+    service.error(defaultMessage, options, true);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: 'report',
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', 'error'],
+      },
+    );
+  });
+
+  it('should send a warn snackbar', () => {
+    service.warn(defaultMessage);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: undefined,
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', 'warn'],
+      },
+    );
+  });
+
+  it('should send a warn snackbar with defaultIcon', () => {
+    service.warn(defaultMessage, undefined, true);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: 'warning',
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', 'warn'],
+      },
+    );
+  });
+
+  it('should send a warn snackbar with options', () => {
+    const options: SnackbarOptions = {
+      variant: 'outlined',
+      icon: 'bug_report',
+    };
+    service.warn(defaultMessage, options, false);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: options.icon,
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', options.variant, 'warn'],
+      },
+    );
+  });
+
+  it('should send a warn snackbar with default icon overriding options', () => {
+    const options: SnackbarOptions = {
+      icon: 'bug_report',
+    };
+    service.warn(defaultMessage, options, true);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: 'warning',
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', 'warn'],
+      },
+    );
+  });
+
+  it('should send an info snackbar', () => {
+    service.info(defaultMessage);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: undefined,
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', 'info'],
+      },
+    );
+  });
+
+  it('should send an info snackbar with defaultIcon', () => {
+    service.info(defaultMessage, undefined, true);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: 'info',
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', 'info'],
+      },
+    );
+  });
+
+  it('should send an info snackbar with options', () => {
+    const options: SnackbarOptions = {
+      variant: 'outlined',
+      icon: 'bug_report',
+    };
+    service.info(defaultMessage, options, false);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: options.icon,
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', options.variant, 'info'],
+      },
+    );
+  });
+
+  it('should send an info snackbar with default icon overriding options', () => {
+    const options: SnackbarOptions = {
+      icon: 'bug_report',
+    };
+    service.info(defaultMessage, options, true);
+
+    expect(snackbarMock.openFromComponent).toHaveBeenCalledWith(
+      SnackbarComponent,
+      {
+        data: {
+          message: defaultMessage,
+          icon: 'info',
+          buttonText: undefined,
+        },
+        direction: undefined,
+        duration: undefined,
+        horizontalPosition: defaultHorizontalPosition,
+        verticalPosition: defaultVerticalPosition,
+        panelClass: ['icon', 'info'],
+      },
+    );
+  });
+
+  it('should clear the snackbar', () => {
+    service.clear();
+
+    expect(snackbarMock.dismiss).toHaveBeenCalled();
+  });
+});
---
+++ libs/angular/shared/data-access/src/lib/snackbar/snackbar.service.ts
@@ -0,0 +1,69 @@
+import { inject, Injectable } from '@angular/core';
+import {
+  MatSnackBar,
+  MatSnackBarConfig,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+import { SnackbarComponent } from '@jdw/angular-shared-ui';
+import { PaletteColors, SnackbarOptions } from '@jdw/angular-shared-util';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SnackbarService {
+  private duration = 3000;
+  private horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  private verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+  private snackbar = inject(MatSnackBar);
+
+  send(type: PaletteColors, message: string, options?: SnackbarOptions) {
+    const panelClass = [];
+    if (!options?.buttonText) panelClass.push('icon');
+    if (options?.variant) panelClass.push(options.variant);
+
+    const config: MatSnackBarConfig = {
+      data: {
+        message: message,
+        icon: options?.icon,
+        buttonText: options?.buttonText,
+      },
+      direction: options?.direction,
+      duration: options?.autoClose
+        ? options?.autoCloseTimeout
+          ? options.autoCloseTimeout
+          : this.duration
+        : undefined,
+      horizontalPosition:
+        options?.horizontalPosition || this.horizontalPosition,
+      verticalPosition: options?.verticalPosition || this.verticalPosition,
+      panelClass: [...panelClass, type],
+    };
+
+    this.snackbar.openFromComponent(SnackbarComponent, config);
+  }
+
+  success(message: string, options?: SnackbarOptions, defaultIcon = false) {
+    if (defaultIcon) options = { ...options, icon: 'check_circle' };
+    this.send('success', message, options);
+  }
+
+  error(message: string, options?: SnackbarOptions, defaultIcon = false) {
+    if (defaultIcon) options = { ...options, icon: 'report' };
+    this.send('error', message, options);
+  }
+
+  warn(message: string, options?: SnackbarOptions, defaultIcon = false) {
+    if (defaultIcon) options = { ...options, icon: 'warning' };
+    this.send('warn', message, options);
+  }
+
+  info(message: string, options?: SnackbarOptions, defaultIcon = false) {
+    if (defaultIcon) options = { ...options, icon: 'info' };
+    this.send('info', message, options);
+  }
+
+  clear() {
+    this.snackbar.dismiss();
+  }
+}
