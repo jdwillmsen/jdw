6803e9f58ce36ab443f11e4b688f95b48e44d452
Jake Willmsen <jdwillmsen@gmail.com>
2024-09-28 16:59:58 -0500

    feat(container): add environment config updates

--- apps/angular/container/container/src/app/app.component.spec.ts
+++ apps/angular/container/container/src/app/app.component.spec.ts
@@ -2,8 +2,8 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { ENVIRONMENT } from '@jdw/angular-container-util';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ENVIRONMENT } from '@jdw/angular-shared-util';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
--- apps/angular/container/container/src/app/app.config.ts
+++ apps/angular/container/container/src/app/app.config.ts
@@ -2,8 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { ENVIRONMENT } from '@jdw/angular-container-util';
 import { provideHttpClient } from '@angular/common/http';
+import { ENVIRONMENT } from '@jdw/angular-shared-util';
 import config from '../config.json';
 
 export const appConfig: ApplicationConfig = {
