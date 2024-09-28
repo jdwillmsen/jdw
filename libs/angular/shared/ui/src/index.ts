4ab8d9760f5b58f2497ee1758f204de872247954
Jake Willmsen <jdwillmsen@gmail.com>
2024-09-28 16:52:11 -0500

    feat(angular-shared-ui): add export for snackbar component

--- libs/angular/shared/ui/src/index.ts
+++ libs/angular/shared/ui/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/header/header.component';
 export * from './lib/navigation-layout/navigation-layout.component';
 export * from './lib/navigation-item/navigation-item.component';
+export * from './lib/snackbar/snackbar.component';
