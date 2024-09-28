a0bd5cf01270a5f2cf3fd469d94f6145093f0f92
Jake Willmsen <jdwillmsen@gmail.com>
2024-09-28 16:55:42 -0500

    chore(angular-container-feature-core): update main component dependencies for environment token

--- libs/angular/container/feature/core/src/lib/main/main.component.cy.ts
+++ libs/angular/container/feature/core/src/lib/main/main.component.cy.ts
@@ -1,9 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 import { MainComponent } from './main.component';
-import { ENVIRONMENT } from '@jdw/angular-container-util';
 import { ActivatedRoute } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ENVIRONMENT } from '@jdw/angular-shared-util';
 
 describe(MainComponent.name, () => {
   beforeEach(() => {
--- libs/angular/container/feature/core/src/lib/main/main.component.spec.ts
+++ libs/angular/container/feature/core/src/lib/main/main.component.spec.ts
@@ -2,8 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MainComponent } from './main.component';
 import { ActivatedRoute } from '@angular/router';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { ENVIRONMENT } from '@jdw/angular-container-util';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ENVIRONMENT } from '@jdw/angular-shared-util';
 
 describe('MainComponent', () => {
   let component: MainComponent;
