3fe53cc42addc7e9d3c486c8e5ba58fd1561e688
Jake Willmsen <jdwillmsen@gmail.com>
2024-09-28 20:36:19 -0500

    feat(container): add fallback for auth remote module

--- apps/angular/container/container/src/app/app.routes.ts
+++ apps/angular/container/container/src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Route } from '@angular/router';
 import { angularContainerFeatureCoreRoutes } from '@jdw/angular-container-feature-core';
 import { loadRemoteModule } from '@nx/angular/mf';
+import { FallbackComponent } from '@jdw/angular-container-feature-core';
 
 export const appRoutes: Route[] = [
   {
-    path: 'authui',
+    path: 'auth',
     loadChildren: () =>
-      loadRemoteModule('authui', './Routes').then((m) => m.remoteRoutes),
+      loadRemoteModule('authui', './Routes')
+        .then((m) => m.remoteRoutes)
+        .catch(() => [{ path: '**', component: FallbackComponent }]),
   },
   {
     path: '',
