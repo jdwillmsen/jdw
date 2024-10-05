c439a4dc62c77a5c55ff154988d3754757274034
Jake Willmsen <jdwillmsen@gmail.com>
2024-10-04 22:10:11 -0500

    feat(container): add service discovery module federation config loading
    Adds config from the service discovery app with a fallback option and cors and mime config updates.

--- apps/angular/container/container/src/main.ts
+++ apps/angular/container/container/src/main.ts
@@ -1,6 +1,18 @@
 import { setRemoteDefinitions } from '@nx/angular/mf';
 
-fetch('/assets/module-federation.manifest.json')
-  .then((res) => res.json())
+import fallbackDefinitions from './assets/module-federation.manifest.json';
+import config from './config.json';
+
+fetch(`${config.SERVICE_DISCOVERY_BASE_URL}/config`)
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error(`Error fetching config: ${res.statusText}`);
+    }
+    return res.json();
+  })
   .then((definitions) => setRemoteDefinitions(definitions))
+  .catch((err) => {
+    console.error('Fetch failed, applying fallback config:', err);
+    setRemoteDefinitions(fallbackDefinitions);
+  })
   .then(() => import('./bootstrap').catch((err) => console.error(err)));
