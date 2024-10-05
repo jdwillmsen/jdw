82634dd22e611ac0948423b1d963de2e1e7af766
Jake Willmsen <jdwillmsen@gmail.com>
2024-10-04 21:57:20 -0500

    feat(angular-shared-util): add service discovery environment variable to token type

--- libs/angular/shared/util/src/lib/environment.token.ts
+++ libs/angular/shared/util/src/lib/environment.token.ts
@@ -3,6 +3,7 @@ import { InjectionToken } from '@angular/core';
 export type Environment = {
   ENVIRONMENT: string;
   AUTH_BASE_URL: string;
+  SERVICE_DISCOVERY_BASE_URL: string;
 } & Record<string, string>;
 
 export const ENVIRONMENT = new InjectionToken<Environment>('ENVIRONMENT');
