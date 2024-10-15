d6904625b1e205a7ae19594a3568a66459b0ee1f
Jake Willmsen <jdwillmsen@gmail.com>
2024-10-14 21:33:29 -0500

    feat(usersui): add http and config setup

--- apps/angular/usersui/usersui/src/app/app.config.ts
+++ apps/angular/usersui/usersui/src/app/app.config.ts
@@ -1,10 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import config from '../config.json';
+import { ENVIRONMENT } from '@jdw/angular-shared-util';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideAnimations(),
     provideRouter(appRoutes),
+    {
+      provide: ENVIRONMENT,
+      useValue: config,
+    },
   ],
 };
