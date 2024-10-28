5e799eeb6f668f997de0faf08f3e3dce73602054
Jake Willmsen <jdwillmsen@gmail.com>
2024-10-27 21:34:54 -0500

    feat(container): add support for dynamic route loading

--- apps/angular/container/container/src/app/app.config.ts
+++ apps/angular/container/container/src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { ENVIRONMENT } from '@jdw/angular-shared-util';
 import config from '../config.json';
+import { appRoutes } from './app.routes';
+import { provideRouter } from '@angular/router';
+import { DynamicRouteLoaderService } from '@jdw/angular-container-data-access';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +15,14 @@ export const appConfig: ApplicationConfig = {
     {
       provide: ENVIRONMENT,
       useValue: config,
+    },
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: (dynamicRouteLoaderService: DynamicRouteLoaderService) => {
+        return () => dynamicRouteLoaderService.loadRoutes();
+      },
+      deps: [DynamicRouteLoaderService],
     },
   ],
 };
