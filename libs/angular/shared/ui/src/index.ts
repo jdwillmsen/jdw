bee6e3f062a16378209b3384382c0b605f76ca80
Jake Willmsen <jdwillmsen@gmail.com>
2024-10-27 21:30:21 -0500

    feat(angular-shared-ui): add fallback component

--- libs/angular/shared/ui/src/index.ts
+++ libs/angular/shared/ui/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/header/header.component';
 export * from './lib/navigation-layout/navigation-layout.component';
 export * from './lib/navigation-item/navigation-item.component';
 export * from './lib/snackbar/snackbar.component';
+export * from './lib/fallback/fallback.component';
