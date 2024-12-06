c38c127b8f357d5384baced521abc50ede9a555a
Jake Willmsen <jdwillmsen@gmail.com>
2024-12-05 23:50:25 -0600

    test(angular-usersui-feature-core): add profiles action button cell renderer unit test fix

--- libs/angular/usersui/feature/core/src/lib/profiles-action-button-cell-renderer/profiles-action-button-cell-renderer.component.spec.ts
+++ libs/angular/usersui/feature/core/src/lib/profiles-action-button-cell-renderer/profiles-action-button-cell-renderer.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProfilesActionButtonCellRendererComponent } from './profiles-action-button-cell-renderer.component';
+import { ActivatedRoute } from '@angular/router';
 
 describe('ProfilesActionButtonCellRendererComponent', () => {
   let component: ProfilesActionButtonCellRendererComponent;
@@ -8,6 +9,12 @@ describe('ProfilesActionButtonCellRendererComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProfilesActionButtonCellRendererComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: ActivatedRoute,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(
