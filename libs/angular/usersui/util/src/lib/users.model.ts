83a4d1fc00cf25cec0297d7c0ba96b0c4871da7f
Jake Willmsen <jdwillmsen@gmail.com>
2025-02-23 16:27:51 -0600

    feat(angular-usersui-util): add address request type

--- libs/angular/usersui/util/src/lib/users.model.ts
+++ libs/angular/usersui/util/src/lib/users.model.ts
@@ -15,6 +15,15 @@ export type Address = {
   modifiedTime: string;
 };
 
+export type AddressRequest = {
+  addressLine1: string;
+  addressLine2: string;
+  city: string;
+  stateProvince: string;
+  postalCode: string;
+  country: string;
+};
+
 export type Icon = {
   id: number;
   profileId: number;
