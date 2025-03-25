ef8ff656f5a8543d676430a22935fcd7e182fa5e
Jake Willmsen <jdwillmsen@gmail.com>
2025-03-25 01:50:11 -0500

    feat(rolesui): add rolesui app

---
+++ apps/angular/rolesui/rolesui/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Route } from '@angular/router';
+
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./remote-entry/entry.routes').then((m) => m.remoteRoutes),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
