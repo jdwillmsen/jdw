b548676fff48a6970e45605ecb2d5fa2c4b752d0
Jake Willmsen <jdwillmsen@gmail.com>
2025-03-24 21:16:33 -0500

    feat(angular-usersui-feature-core): add additional routing

--- libs/angular/usersui/feature/core/src/lib/lib.routes.ts
+++ libs/angular/usersui/feature/core/src/lib/lib.routes.ts
@@ -4,6 +4,9 @@ import { UserComponent } from './user/user.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfilesComponent } from './profiles/profiles.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AddressComponent } from './address/address.component';
+import { AccountComponent } from './account/account.component';
+import { IconComponent } from './icon/icon.component';
 
 export const angularUsersuiFeatureCoreRoutes: Route[] = [
   { path: '', component: DashboardComponent },
@@ -11,5 +14,25 @@ export const angularUsersuiFeatureCoreRoutes: Route[] = [
   { path: 'users', component: UsersComponent },
   { path: 'profile/:userId', component: ProfileComponent },
   { path: 'profiles', component: ProfilesComponent },
+  {
+    path: 'profile/:profileId/address/:addressId',
+    component: AddressComponent,
+  },
+  {
+    path: 'profile/:profileId/address',
+    component: AddressComponent,
+  },
+  {
+    path: 'account/:userId',
+    component: AccountComponent,
+  },
+  {
+    path: 'account',
+    component: AccountComponent,
+  },
+  {
+    path: 'profile/:profileId/icon',
+    component: IconComponent,
+  },
   { path: '**', redirectTo: '' },
 ];
