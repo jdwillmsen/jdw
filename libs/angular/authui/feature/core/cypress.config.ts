bb9c8deccacdec844011f02f7ea341d76884d969
Jake Willmsen <42048994+jdwillmsen@users.noreply.github.com>
2025-05-27 00:08:51 -0500

    fix: update package-lock.json

--- libs/angular/authui/feature/core/cypress.config.ts
+++ libs/angular/authui/feature/core/cypress.config.ts
@@ -4,9 +4,6 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   component: {
     ...nxComponentTestingPreset(__filename),
-    // Please ensure you use `cy.origin()` when navigating between domains and remove this option.
-    // See https://docs.cypress.io/app/references/migration-guide#Changes-to-cyorigin
-    injectDocumentDomain: true,
   },
   port: 9000,
 });
--- libs/angular/container/feature/core/cypress.config.ts
+++ libs/angular/container/feature/core/cypress.config.ts
@@ -6,9 +6,6 @@ export default defineConfig({
     ...nxComponentTestingPreset(__filename, {
       buildTarget: 'container:build',
     }),
-    // Please ensure you use `cy.origin()` when navigating between domains and remove this option.
-    // See https://docs.cypress.io/app/references/migration-guide#Changes-to-cyorigin
-    injectDocumentDomain: true,
   },
   port: 9001,
 });
